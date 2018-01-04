6473ff5d18b8bea840ad473324f5aec8c615101d
rachelruff <33016102+rachelruff@users.noreply.github.com>
2018-01-04 16:04:40 -0600

    Finished

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const strategy = require(`${__dirname}/strategy.js`);
+const request = require("request");
 
 const app = express();
-app.use( session({
-  secret: '@nyth!ng y0u w@nT',
-  resave: false,
-  saveUninitialized: false
-}));
+
+app.use(
+  session({
+    secret: "@nyth!ng y0u w@nT",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  const { _json } = user;
+  return done(null, {
+    clientID: _json.clientID,
+    email: _json.email,
+    name: _json.name,
+    followers: _json.followers_url
+  });
+});
+
+passport.deserializeUser(function(obj, done) {
+  return done(null, obj);
+});
+
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/followers",
+    failRedirect: "/login",
+    failureFlash: true,
+    connection: "github"
+  })
+);
+app.get("/followers", function(req, res, next) {
+  if (req.user) {
+    const FollowersRequest = {
+      url: req.user.followers,
+      headers: {
+        "User-Agent": req.user.clientID
+      }
+    };
+    request(FollowersRequest, (error, response, body) => {
+      res.status(200).send(body);
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
 
 const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ strategy.js
@@ -0,0 +1,14 @@
+const Auth0Strategy = require("passport-auth0");
+const { domain, clientID, clientSecret } = require("./config.js");
+
+module.exports = new Auth0Strategy(
+  {
+    domain: domain,
+    clientID: clientID,
+    clientSecret: clientSecret,
+    callbackURL: "/login"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
+);
